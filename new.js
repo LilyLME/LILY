9f2802d1858a3f78c551bb7355442181f6faf895
LilyLME <brilyjoy@gmail.com>
2018-05-04 12:30:01 +0100

    first commit

---
+++ new.js
@@ -0,0 +1,5 @@
+ var N = prompt(" enter :");
+   var Z = prompt ("Enter ");
+  var X=  prompt ("Enter ")  ;
+  var Y= prompt ("Enter ") ;
+  console.log("you will be a " + X + " in " + Y + " ,and married to "  + Z + " with " + N + " kids ");
---
+++ newassignment.js
@@ -0,0 +1,5 @@
+ var N = prompt(" enter :")
+   var Z = rich,handsome,nice man;
+  var X= Web DESIGNER;
+  var Y=2019;
+  console.log("you will be a " + X + " in " + Y + " ,and married to "  + Z + " with N kids "
